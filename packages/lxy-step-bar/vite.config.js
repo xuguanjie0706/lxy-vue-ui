860b1c7b9eb9367b9f4a232d50d8348d44787954
xuguanjie <215790428@qq.com>
2024-04-27 17:25:37 +0800

    修改配置

--- packages/lxy-step-bar/vite.config.js
+++ packages/lxy-step-bar/vite.config.js
@@ -1,7 +1,8 @@
 // vite.config.js
-import vue from '@vitejs/plugin-vue2';
-import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import vue from '@vitejs/plugin-vue2'
+import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+// import { viteExternalsPlugin } from 'vite-plugin-externals'
 export default defineConfig({
   // 配置选项
   plugins: [vue(), cssInjectedByJsPlugin({ topExecutionPriority: false })],
@@ -10,9 +11,19 @@ export default defineConfig({
       entry: 'src/main.js',
       formats: ['umd', 'iife', 'cjs'],
       name: 'index',
-      fileName: 'index',
+      fileName: 'index'
     },
     outDir: 'dist',
-    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
-  },
-});
+    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14']
+  }
+  // rollupOptions: {
+  //   // 告诉打包工具 在external配置的 都是外部依赖项  不需要打包
+  //   external: ['vue'],
+  //   plugins: [
+  //     externalGlobals({
+  //       // "在项目中引入的变量名称"："CDN包导出的名称，一般在CDN包中都是可见的"
+  //       vue: 'Vue'
+  //     })
+  //   ]
+  // }
+})
