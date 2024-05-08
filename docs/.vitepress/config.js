8a8df687b4e24cb45059f03c5437e926fb63970a
xuguanjie <215790428@qq.com>
2024-05-08 21:11:21 +0800

    代码提及

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -147,6 +147,7 @@ export default {
               link: '/daily/pkg/pnpm/index.md',
             },
             {
+              
               text:"代码规范",
               link: '/daily/pkg/husky/index.md',
             }
